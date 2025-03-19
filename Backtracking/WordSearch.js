91cecbc886fa6053a3b5007905a95c1c135e8580
papinabiswal <39021982+papinabiswal@users.noreply.github.com>
2025-03-19 17:24:29 +0000

    word search

---
+++ Backtracking/WordSearch.js
@@ -0,0 +1,32 @@
+var exist = function(board, word) {
+    const m = board.length;
+    const n = board[0].length;
+
+    const backtrack = (i, j, idx) => {
+        if(idx === word.length) return true;
+
+        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== word[idx]){
+           return false;
+        }
+        // common backtrack part
+        let temp = board[i][j];
+        board[i][j] = '$';
+         
+        const result = backtrack(i+1, j, idx+1) ||
+                       backtrack(i-1, j, idx+1) ||
+                       backtrack(i, j+1, idx+1) ||
+                       backtrack(i, j-1, idx+1);
+
+        board[i][j] = temp;
+        return result;
+    }
+
+     for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if(backtrack(i,j,0)){
+                return true;
+            }
+        }
+    }
+    return false;
+};
