c21b397580f51709aeb73af07734a7afec97fad7
papinabiswal <39021982+papinabiswal@users.noreply.github.com>
2025-03-30 11:47:02 +0530

    fixed conflict

--- String/4IntegerToRoman.js
+++ String/4IntegerToRoman.js
@@ -25,6 +25,23 @@ function intToRoman(num){
 }
 intToRoman(58);
 
+// OR
+
+function solution(num){
+    const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const sym = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    let result ='';
+    for(let i=0; i< val.length; i++){
+        while(val[i] <= num){
+            num -= val[i];
+            result += sym[i];
+        }
+    }
+    return result;
+}
+solution(58);
+
 //OR
 function intToRoman(num) {
     const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
