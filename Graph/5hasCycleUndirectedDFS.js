ddb841355be22c28bb6edcf1058d7f2698366a6c
papinabiswal <39021982+papinabiswal@users.noreply.github.com>
2025-04-06 15:43:24 +0000

    dfs recursive and iterative

--- Graph/5hasCycleUndirectedDFS.js
+++ Graph/5hasCycleUndirectedDFS.js
@@ -1,97 +1,37 @@
-// recursive approach
-class Graph {
-    constructor(){
-        this.adjancyList = {};
+function dfsRecursive(V, edges){
+    let adj = new Array(V).fill(0).map(()=>[]);
+     
+    for(let [u,v] of edges){
+        adj[u].push(v);
+        adj[v].push(u);
     }
-
-    //
-    addVertex(vertex){
-        if(!this.adjancyList[vertex]){
-            this.adjancyList[vertex] = [];
-        }
-    }
-
-    addEdge(vertex1, vertex2, isDirected=false){
-        if(!this.adjancyList[vertex1]) this.addVertex(vertex1);
-        if(!this.adjancyList[vertex2]) this.addVertex(vertex2);
-
-        this.adjancyList[vertex1].push(vertex2);
-        if(!isDirected){
-         this.adjancyList[vertex2].push(vertex1);
-        }
-        
-    }
-
-    hasCycleUndirected(){
-        const visited = new Set();
-
-        const dfs = (node, parent)=>{
-            visited.add(node);
-
-            for(let neighbour of graph.adjancyList[node]){
-                if(!visited.has(neighbour)){
-                    if(dfs(neighbour, node)) return true;
-                } else {
-                    if(neighbour !== parent){
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        for(let vertex in this.adjancyList){
-            if(!visited.has(vertex)){
-                // The -1 means: you didn’t come from anywhere (because this is your first plant in the new section).
-               // If any DFS finds a cycle (true), return true right away.
-                if(dfs(vertex, -1)) return true;
-            }
-        }
-        return false;
-    }
-    
-}
-
-let graph = new Graph();
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addVertex("D");
-graph.addVertex("E");
-graph.addEdge("A", "B");
-graph.addEdge("B", "C");
-graph.addEdge("C", "A");
-graph.addEdge("D", "E");
-
-class Solution {
-    isCycle(V, edges) {
-        let adj = new Array(V).fill(0).map(()=> []);
-        
-        // build edge
-        for(let [u, v] of edges){
-            adj[u].push(v);
-            adj[v].push(u);
-        }
-        
-        let visited = new Set();
-        
-        const dfs = (start, parent)=>{
-            visited.add(start);
-            
-            for(let neighbour of adj[start]){
-                if(!visited.has(neighbour)){
-                    if(dfs(neighbour, start)) return true;
-                } else if(neighbour !== parent){
-                    return true;
-                }
-            }
-            return false;
-        }
-        
-        for(let i=0; i< V; i++){
-            if(!visited[i]){
-                if(dfs(i, -1)) return true;
-            }
-        }
-        return false;
-    }
-}
+     let visited = new Array(V).fill(false);
+ 
+     function dfs(start, parent){
+         visited[start] = true;
+         for(let neighbour of adj[start]){
+             if(!visited[neighbour]){
+                 if(dfs(neighbour, start)) return true;
+             } else if(neighbour !== parent){
+                 return true;
+             }
+         }
+         return false;
+     }
+     for(let i=0; i< V; i++){
+         if(!visited[i]){
+             if(dfs(i, -1)) return true;
+         }
+     }
+     return false;
+ }
+ 
+ let V1 = 6;
+ let edges1 = [
+     [0, 1],
+     [0, 2],
+     [1, 3],
+     [1, 4],
+     [2, 1]
+ ];
+ dfsRecursive(V1, edges1);
--- Graph/7hasCycleUndirectedIterativeBFS.js
+++ Graph/7hasCycleUndirectedIterativeBFS.js
@@ -1,36 +1,38 @@
-class Solution {
-    isCycle(V, edges) {
-        let adj = new Array(V).fill(0).map(() => []);
+function bfsIterative(V, edges){
+    let adj = new Array(V).fill(0).map(()=> []);
+    for(let [u, v] of edges){
+        adj[u].push(v);
+        adj[v].push(u);
+    }
+    let visited = new Array(V).fill(false);
 
-        // Build adjacency list
-        for (let [u, v] of edges) {
-            adj[u].push(v);
-            adj[v].push(u);
-        }
+    for(let i=0; i<V; i++){
+        if(!visited[i]){
+            let queue = [[i, -1]];
+            visited[i] = true;
 
-        let visited = new Set();
-
-        for (let i = 0; i < V; i++) {
-            if (!visited.has(i)) {
-                let queue = [[i, -1]];
-
-                while (queue.length > 0) {
-                    let [node, parent] = queue.shift();
-
-                    visited.add(node);
-
-                    for (let neighbour of adj[node]) {
-                        if (!visited.has(neighbour)) {
-                            queue.push([neighbour, node]);
-                        } else if (neighbour !== parent) {
-                            // already visited and not parent → cycle
-                            return true;
-                        }
+            while(queue.length > 0){
+                let [node, parent] = queue.shift();
+                for(let neighbour of adj[node]){
+                    if(!visited[neighbour]){
+                        queue.push([neighbour, node]);
+                        visited[neighbour] = true;
+                    } else if(neighbour !== parent){
+                        return true;
                     }
                 }
             }
         }
-
-        return false;
     }
+    return false;
 }
+
+let V1 = 6;
+let edges1 = [
+    [0, 1],
+    [0, 2],
+    [1, 3],
+    [1, 4],
+    [2, 1]
+];
+bfsIterative(V1, edges1);
