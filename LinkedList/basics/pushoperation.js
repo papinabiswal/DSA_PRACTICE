812ffad2da37f4723b09d9a8a62d1a3c52c3a0c1
Papina Biswal <papinabiswal@LMOLMACLTGCC4NRL.local>
2025-06-02 09:57:59 +0530

    basic linked list

---
+++ LinkedList/basics/pushoperation.js
@@ -0,0 +1,116 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+var first = new Node(10);
+first.next = new Node(20);
+first.next.next = new Node(30);
+first.next.next.next = new Node(40);
+
+// type in console first
+// type in console first.next 
+// you can get the corresponding value.
+
+// *******************************************************************************************************************************
+
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SingleLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length =0;
+    }
+    push(val) {
+         let newNode = new ListNode(val);
+         if(!this.head){
+            this.head  = newNode;
+            this.tail = newNode;
+         } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+         }
+         this.length++;
+         return this;
+    }
+
+    pop() {
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        return current;
+    }
+    shift() {
+        if(!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    unshift(val) {
+        let newNode = new ListNode(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while(counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    set(index, val) { // set the value of the node at the given index to the given value
+        let foundNode = this.get(index);
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    insert(index, val) {
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+
+        let newNode = new ListNode(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+}
+
+let list = new SingleLinkedList();
+list.push(10);
+list.push(20);
+list.push(30);
+list.push(40);
