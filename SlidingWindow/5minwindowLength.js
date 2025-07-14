eb994359fdbcaf373b26fadbe7dbc901ea1f0d10
Papina Biswal <papinabiswal@LMOLMACLTGCC4NRL.local>
2025-07-14 22:24:44 +0530

    min window length

--- SlidingWindow/5minwindowLength.js
+++ SlidingWindow/5minwindowLength.js
@@ -1,32 +1,46 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
 var minWindow = function(s, t) {
-    let count = new Array(256).fill(0); // both caps and small letter
-   for(let i = 0; i < t.length; i++) {
-       count[t.charCodeAt(i)]++;
-   }
+    const count = {};
+    
+    // Build frequency map for string t
+    for (let char of t) {
+        count[char] = (count[char] || 0) + 1;
+    }
 
-   let requiredCount = t.length;
-   let i = 0;
-   let j = 0;
-   let minL = Infinity;
-   let start = -1;
-   while(j < s.length) {
-       if(count[s.charCodeAt(j)] > 0) {
-           requiredCount--;
-       }
-       count[s.charCodeAt(j)]--;
-       while(requiredCount === 0) {
-           count[s.charCodeAt(i)]++;
-           if(count[s.charCodeAt(i)] > 0) {
-               requiredCount++;
-           }
-           if(minL > j - i + 1) {
-               minL = j - i + 1;
-               start = i;
-           }
-           i++;
-       }
-       j++;
-   }
-   return start === -1 ? '' : s.slice(start, start + minL);
+    let requiredCount = t.length;
+    let i = 0, j = 0;
+    let minL = Infinity;
+    let start = -1;
 
+    while (j < s.length) {
+        let endChar = s[j];
+        if (count[endChar] > 0) {
+            requiredCount--;
+        }
+        count[endChar] = (count[endChar] || 0) - 1;
+
+        // Try to shrink the window from the left
+        while (requiredCount === 0) {
+            if (j - i + 1 < minL) {
+                minL = j - i + 1;
+                start = i;
+            }
+
+            let startChar = s[i];
+            count[startChar] = (count[startChar] || 0) + 1;
+            if (count[startChar] > 0) {
+                requiredCount++;
+            }
+
+            i++;
+        }
+
+        j++;
+    }
+
+    return start === -1 ? '' : s.slice(start, start + minL);
 };
